40e59646389bed1f9934d37e905834fd078eaf97
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-01-27 14:50:37 -0300

    Add files via upload

---
+++ aulas/src/aulas/exemplos.java
@@ -0,0 +1,18 @@
+package aulas;
+
+public class exemplos {
+
+	public static void main(String[] args) {
+		
+		int[] nota = {9,8,10};
+		
+		
+		for(int valor: nota) {
+			System.out.println(valor);
+		}
+		
+		
+
+	}
+
+}
